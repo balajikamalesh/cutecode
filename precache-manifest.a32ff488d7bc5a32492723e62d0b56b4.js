6a8d8e9aae8c3ffa928f5582ce5e4269f4b24cc2
Balaji <abalajikamalesh@gmail.com>
2020-09-28 11:38:41 +0530

    Updates

---
+++ precache-manifest.a32ff488d7bc5a32492723e62d0b56b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5926cbcc991f2eb9c1fe0b3ff1bbbd11",
+    "url": "/cutecode/index.html"
+  },
+  {
+    "revision": "f8e7e70a443e57666517",
+    "url": "/cutecode/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "93beebb725eda1192335",
+    "url": "/cutecode/static/css/main.8fa11203.chunk.css"
+  },
+  {
+    "revision": "f8e7e70a443e57666517",
+    "url": "/cutecode/static/js/2.a93765c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cutecode/static/js/2.a93765c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93beebb725eda1192335",
+    "url": "/cutecode/static/js/main.ffb9b4c5.chunk.js"
+  },
+  {
+    "revision": "07d0d6e675d496cbb0b2",
+    "url": "/cutecode/static/js/runtime-main.a510bf0f.js"
+  }
+]);
